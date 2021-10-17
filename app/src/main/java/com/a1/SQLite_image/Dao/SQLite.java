4b12beb0e33c44e6595d7ce0781180e1cf222b0a
YinQin <2774247046@qq.com>
2021-10-18 07:51:44 +0800

    lxs

---
+++ app/src/main/java/com/a1/SQLite_image/Dao/SQLite.java
@@ -0,0 +1,32 @@
+package com.a1.SQLite_image.Dao;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+public class SQLite extends SQLiteOpenHelper{
+    public SQLite(@Nullable Context context){
+        super(context, "image.db", null, 4);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table a(" +
+                "image BLOB," +
+                "name varchar," +
+                "id Integer)");
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+        db.execSQL("drop table if exists a ");
+        onCreate(db);
+    }
+    public Cursor All(String[] name){
+        SQLiteDatabase data=getWritableDatabase();
+        return data.rawQuery("select*from a where name=?",name);
+    }
+    public void Add(byte []a,String[]name){
+        SQLiteDatabase sqLiteDatabase=getWritableDatabase();
+        sqLiteDatabase.execSQL("insert into a(image,name,id)values(?,?,?)",new Object[]{a,name[0],name[1]});
+    }
+}
---
+++ app/src/main/java/com/a1/SQLite_image/Main.java
@@ -0,0 +1,77 @@
+package com.a1.SQLite_image;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.a1.SQLite_image.Dao.SQLite;
+import com.example.lxs.R;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.sql.Date;
+
+public class Main extends AppCompatActivity implements View.OnClickListener{
+    private Button a1_button_add,a1_button_all,a1_button_delete;
+    private ImageView image,image_1,image_2;
+    private SQLite sqLite=new SQLite(this);
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a1_main);
+        a1_button_add=findViewById(R.id.a1_button_add);
+        a1_button_all=findViewById(R.id.a1_button_All);
+        a1_button_delete=findViewById(R.id.a1_button_delete);
+        image_1=findViewById(R.id.a1_image_1);
+        image_2=findViewById(R.id.a1_image_2);
+        image=findViewById(R.id.a1_image);
+        a1_button_add.setOnClickListener(this);
+        a1_button_all.setOnClickListener(this);
+        a1_button_delete.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.a1_button_All:
+                String []name={"第二张图片"};
+                Cursor cursor=sqLite.All(name);
+                if (cursor!=null){
+                    if (cursor.moveToFirst()){
+                        do {
+                            byte []i=cursor.getBlob(cursor.getColumnIndex("image"));
+                            Bitmap bitmap=BitmapFactory.decodeByteArray(i,0,i.length);
+                            Log.d("bitmap",bitmap+"");
+                            image.setImageBitmap(bitmap);
+                            image_1.setImageBitmap(bitmap);
+                            image_2.setImageBitmap(bitmap);
+                        }while (cursor.moveToNext());
+                    }
+                }
+                break;
+            case R.id.a1_button_delete:
+
+                break;
+            case R.id.a1_button_add:
+                String []a={"第一张图片","5"};
+                sqLite.Add(bitmapToBytes(R.drawable.th1),a);
+                break;
+        }
+    }
+    private byte[] bitmapToBytes(int image_id){
+        ByteArrayOutputStream outputStream=new ByteArrayOutputStream();
+        Bitmap bitmap= BitmapFactory.decodeResource(getResources(),image_id);
+        bitmap.compress(Bitmap.CompressFormat.PNG,100,outputStream);
+        return outputStream.toByteArray();
+    }
+}
