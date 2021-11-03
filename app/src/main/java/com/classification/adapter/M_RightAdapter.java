9132b99597a161dc1d7eeeb9367034cd7cd00bfc
YinQin <2774247046@qq.com>
2021-11-03 10:52:35 +0800

    lxs

--- app/src/main/java/com/classification/adapter/M_RightAdapter.java
+++ app/src/main/java/com/classification/adapter/M_RightAdapter.java
@@ -14,30 +14,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class M_RightAdapter extends BaseAdapter {
-    
     private Context context;
     private List<String> list;
-
     public M_RightAdapter(Context context, List<String> list) {
         this.context = context;
         this.list = list;
     }
-
     @Override
     public int getCount() {
         return list.size();
     }
-
     @Override
     public Object getItem(int position) {
         return list.get(position);
     }
-
     @Override
     public long getItemId(int position) {
         return position;
     }
-
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         Holder holder=new Holder();
--- app/src/main/java/com/toolbar/Fragment1.java
+++ app/src/main/java/com/toolbar/Fragment1.java
@@ -6,10 +6,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.lxs.R;
 
@@ -25,9 +28,42 @@ public class Fragment1 extends Fragment {
         for (int i=1;i<=100;i++){
             list.add("测试数据"+i);
         }
-        ListView scroll_ListView=view.findViewById(R.id.scroll_ListView);
-        ArrayAdapter<String> adapter=new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1,list);
-        scroll_ListView.setAdapter(adapter);
+        RecyclerView scroll_ListView=view.findViewById(R.id.scroll_ListView);
+        R_adapter r_adapter=new R_adapter(list);
+        scroll_ListView.setAdapter(r_adapter);
+        scroll_ListView.setLayoutManager(new GridLayoutManager(getContext(),2));
         return view;
+    }
+    class R_adapter extends RecyclerView.Adapter<R_adapter.Holder>{
+        private List<String>list;
+
+        public R_adapter(List<String> list) {
+            this.list = list;
+        }
+
+        @NonNull
+        @Override
+        public Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view=LayoutInflater.from(getContext()).inflate(R.layout.t_recyclerview_item,parent,false);
+            return new Holder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull Holder holder, int position) {
+            holder.textView.setText(list.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return list.size();
+        }
+
+        class Holder extends RecyclerView.ViewHolder{
+            TextView textView;
+            public Holder(@NonNull View itemView) {
+                super(itemView);
+                textView=itemView.findViewById(R.id.t_RecyclerView_itemText);
+            }
+        }
     }
 }
